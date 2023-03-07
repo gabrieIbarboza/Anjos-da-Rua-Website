526f86ef9766bf9f2617a6c64318cbd2ff0f3c3f
Gabriel Klark <gabrielklarkgk@gmail.com>
2023-03-07 00:28:51 -0300

    add menu-btn toggle

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,37 +1,69 @@
 import './Menu.scss';
 
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export function AppMenu() {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const handleMenuBtnClick = () => {
+
+        setIsActive(current => !current)
+
+    }
+
     return (
         <header>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
+            <div onClick={handleMenuBtnClick} className="menu-btn">
+                <span className="menu-btn__burguer" data-open={isActive}></span>
+            </div>
+
+            <nav className="nav" data-open={isActive}>
+            <ul className="menu-nav" data-open={isActive}>
+                <li className="menu-nav__item" data-open={isActive}>
+                    <Link className="menu-nav__link" to='/'>
+                        Home
+                    </Link>
                 </li>
-                <li>
-                    <Link to='/Calendario'>Calendário</Link>
+                <li className="menu-nav__item" data-open={isActive}>
+                    <Link className="menu-nav__link" to='/Calendario'>
+                        Calendário
+                    </Link>
                 </li>
-                <li>
-                    <Link to='/QuemSomos'>Quem Somos?</Link>
+                <li className="menu-nav__item" data-open={isActive}>
+                    <Link className="menu-nav__link" to='/QuemSomos'>
+                        Quem Somos?
+                    </Link>
                 </li>
-                <li>
-                    <Link to='/Ajudar'>Como Ajudar?</Link>
+                <li className="menu-nav__item" data-open={isActive}>
+                    <Link className="menu-nav__link" to='/Ajudar'>
+                        Como Ajudar?
+                    </Link>
                 </li>
-                <li>
-                    <Link to='/Cadastro'>Cadastro</Link>
+                <li className="menu-nav__item" data-open={isActive}>
+                    <Link className="menu-nav__link" to='/Cadastro'>
+                        Cadastro
+                    </Link>
                 </li>
-                <li>
-                    <Link to='/Blog'>Blog</Link>
+                <li className="menu-nav__item" data-open={isActive}>
+                    <Link className="menu-nav__link" to='/Blog'>
+                        Blog
+                    </Link>
                 </li>
-                <li>
-                    <Link to='/Lojinha'>Lojinha</Link>
+                <li className="menu-nav__item" data-open={isActive}>
+                    <Link className="menu-nav__link" to='/Lojinha'>
+                        Lojinha
+                    </Link>
                 </li>
-                <li>
-                    <Link to='/Doe'>Doe</Link>
+                <li className="menu-nav__item" data-open={isActive}>
+                    <Link className="menu-nav__link" to='/Doe'>
+                        Doe
+                    </Link>
                 </li>
                 
             </ul>
+            </nav>
 
         </header>
     )
